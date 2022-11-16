f6c17b303228c800d72767d0b0a93da3fc1904a4
e247438 <e247438@edu.sbl.ch>
2022-11-16 14:42:18 +0100

    only jump if space

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function handelKeyPress(ev) {
+function handleKeyPress(ev) {
+
+    if (ev.code === "Space"){
     const player = document.querySelector("#player")
 
-    player.setAttribute("cx", 100* Math.ramdom())
-    player.setAttribute("cy", 100*Math.random())
+    player.setAttribute("cx", 100* Math.random())
+    player.setAttribute("cy", 100* Math.random())
+}
 }
 
 
 
-window.onkeydown = handelKeyPress
+window.onkeydown = handleKeyPress
