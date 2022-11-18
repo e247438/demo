0a60ea28d7c87222efd14c4b01bf0664dc188c4d
e247438 <e247438@edu.sbl.ch>
2022-11-18 09:06:20 +0100

    addItems

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ function handleKeyPress(ev) {
 
     player.setAttribute("cx", 100* Math.random())
     player.setAttribute("cy", 100* Math.random())
+    spawnItem()
 } else if (ev.key === "r"){
     player.setAttribute("fill", "red")
 }
+}
+
+function spawnItem () {
+    const NS = "http://www.w3.org/2000/svg"
+    const item = document.createElementNS(NS, "circle")
+    item.setAttribute("cx", 100*Math.random())
+    item.setAttribute("cy", 20*Math.random()+ 80 )
+    item.setAttribute("r", 2)    
+    item.setAttribute("fill", "black")
+    document.querySelector("svg").appendChild(item)
 }
 
 
