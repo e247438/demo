d091aade21cd12e5ef7f0d2264d0e717c5f934a4
e247438 <e247438@edu.sbl.ch>
2022-11-18 08:18:39 +0100

    added gravity

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ function handleKeyPress(ev) {
     player.setAttribute("cy", 100* Math.random())
 } else if (ev.key === "r"){
     player.setAttribute("fill", "red")
+}else if (ev.key === "g"){
+    player.setAttribute("fill", "green")
+}}
+
+function gameLoop() {
+    const player = document.querySelector("#player")
+    let posY =parseInt (player.getAttribute("cy", ));
+    player.setAttribute("cy", posY +1);
+    window.requestAnimationFrame(gameLoop);
 }
-}
+
+gameLoop()
 
 
-
-window.onkeydown = handleKeyPress
+window.onkeydown = handleKeyPress;
